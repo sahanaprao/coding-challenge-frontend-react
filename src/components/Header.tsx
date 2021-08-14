69b117a59e3c7bf2aeefafd0c4a3c7ade97ed658
Sahana <iamsahana.p@gmail.com>
2021-08-14 10:56:53 +0200

    Added Header

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import classes from './Header.module.css';
+
+const Header = () => {
+    return (
+        <header>
+            <h1 className={classes.center}>Concular Market Place</h1>
+        </header>
+    );
+}
+
+export default Header;
